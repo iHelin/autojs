b4135b8cc5c84397e5bc891dcda1af58cfafb3f0
iHelin <ihelin@outlook.com>
2020-01-12 16:43:42 +0800

    update

--- 快手极速版.js
+++ 快手极速版.js
@@ -18,7 +18,7 @@ function read() {
         if (currentActivity() === "com.yxcorp.gifshow.HomeActivity") {
 
             sleep(500);
-            swipe(device.width / 2, device.height / 2 + 500, device.width / 2, device.height / 2 - 500, random(300, 1000));
+            swipe(device.width / 2, device.height / 2 + 500, device.width / 2, device.height / 2 - 700, random(300, 1000));
 
             toastLog("等待" + waitTime + "秒");
             sleep(waitTime * 1000);
