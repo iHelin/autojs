b8f8177d3d8b64ae412bcdf80b70f15108064d44
linhe2 <linhe2@iflytek.com>
2020-09-20 19:57:12 +0800

    ok

--- wechat_sport.js
+++ wechat_sport.js
@@ -1,34 +1,47 @@
 
+auto.waitFor();
+// if (text('赞我的朋友').exists()) {
+//     // back();
+//     // sleep(random(1000, 3000));
+//     toastLog('111');
+// }else{
+//     toastLog('222');
+// }
+// exit();
 
 start();
 
 function start() {
     while (true) {
-        if (id('text1').text('排行榜').exist()) {
+        if (text('排行榜').exists()) {
             toastLog('当前在排行榜页面');
 
-            let object = id('bjb').find();
+            let object = id('bo_').find();
             if (!object.empty()) {
-
+                toastLog('点赞按钮不为空！');
                 object.forEach(function (currentValue, index) {
+
                     if (currentValue) {
                         let like = currentValue.parent().parent();
 
                         if (like.click()) {
                             toastLog('执行点赞成功！');
-                            sleep(1000, 3000);
                         } else {
                             toastLog('执行点赞失败！');
                         }
+                        sleep(random(1000, 3000));
                     }
 
-                    if (text('赞我的朋友').exist()) {
+                    if (text('赞我的朋友').exists()) {
                         back();
-                        sleep(random(1000, 3000));
+                        sleep(random()*3000);
+                        toastLog('111');
+                        toastLog('index='+index)
                     }
                     if (textEndsWith('的主页')) {
                         back();
                         sleep(random(1000, 3000));
+                        toastLog('222');
                     }
                 })
             } else {
@@ -42,10 +55,11 @@ function start() {
 
         if (text('邀请朋友')) {
             toastLog('已到达底部，准备结束脚本！')
+            exit();
         }
 
         if (id('text1').text('排行榜').exists()) {
-            let rect = id('jw').findOne().bounds();
+            let rect = id('br').findOne().bounds();
             let y2 = rect.bottom;
             swipe(device.width / 2 + random(-100, 100), device.height - 10, device.width + random(-100, 100), y2, random(100, 2000));
         }
--- 天猫农场-福年种福果.js
+++ 天猫农场-福年种福果.js
@@ -1,3 +1,49 @@
+// auto.waitFor();
+// var height = device.height;
+// var width = device.width;
+// // toast("\n设备宽" + width + "\n" + "设备高" + height + "\n" + "手机型号" + device.model + "\n安卓版本" + device.release)
+// // setScreenMetrics(width, height);
+// // toast("设备高" + height);
+// autoplay();
+// function autoplay() {
+//     if (textEndsWith("去签到").exists()) {
+//         textEndsWith("去签到").findOne().click();
+//         sleep(1600);
+//         toast("签到成功");
+//     }
+//     if (textEndsWith("去兑换").exists()) {
+//         textEndsWith("去兑换").findOne().click();
+//         sleep(1600);
+//         toast("兑换成功");
+//     }
+//     sleep(2000);
+//     toast("完成[签到]检测");
+//     while (textEndsWith("去浏览").exists()) {
+//         //要支持的动作
+//         toast("存在去浏览");
+//         textEndsWith("去浏览").findOne().click();
+//         sleep(5000);
+
+//         if (!textContains("今日已达上限").exists()) {
+//             swipe(width / 2, height - 300, width / 2, 100, 1000);
+//             sleep(2500);
+//             swipe(width / 2, height - 300, width / 2, 100, 1000);
+//             sleep(5000);
+//             swipe(width / 2, height - 300, width / 2, 100, 1000);
+//             sleep(5000);
+//             swipe(width / 2, height - 300, width / 2, 100, 1000);
+//             sleep(5000);
+//         }
+
+//         back();
+//         sleep(2000);
+//     }
+//     toast("完成[去浏览]检测");
+//     sleep(2000);
+//     toast("结束");
+// }
+
+
 auto.waitFor();
 var height = device.height;
 var width = device.width;
@@ -9,36 +55,25 @@ function autoplay() {
     if (textEndsWith("去签到").exists()) {
         textEndsWith("去签到").findOne().click();
         sleep(1600);
-        toast("签到成功");
-    }
-    if (textEndsWith("去兑换").exists()) {
-        textEndsWith("去兑换").findOne().click();
-        sleep(1600);
-        toast("兑换成功");
+        toast("签到成功")
     }
     sleep(2000);
     toast("完成[签到]检测");
-    while (textEndsWith("去浏览").exists()) {
+
+
+    while (textEndsWith("去逛逛").exists()) {
         //要支持的动作
-        toast("存在去浏览");
-        textEndsWith("去浏览").findOne().click();
+        toast("存在去逛逛");
+        textEndsWith("去逛逛").findOne().click();
+
+        swipe(width / 2, height - 300, width / 2, 100, 1000);
+        //aitForActivity  sleep(2500);
+        swipe(width / 2, height - 300, width / 2, 100, 1000);
+        sleep(5000);
+        swipe(width / 2, height - 300, width / 2, 100, 1000);
+        sleep(5000);
+        swipe(width / 2, height - 300, width / 2, 100, 1000);
         sleep(5000);
 
-        if (!textContains("今日已达上限").exists()) {
-            swipe(width / 2, height - 300, width / 2, 100, 1000);
-            sleep(2500);
-            swipe(width / 2, height - 300, width / 2, 100, 1000);
-            sleep(5000);
-            swipe(width / 2, height - 300, width / 2, 100, 1000);
-            sleep(5000);
-            swipe(width / 2, height - 300, width / 2, 100, 1000);
-            sleep(5000);
-        }
-
-        back();
-        sleep(2000);
     }
-    toast("完成[去浏览]检测");
-    sleep(2000);
-    toast("结束");
 }
