9258fafbcd95f4ddd9f93a1a03970b63d73a1b4b
iHelin <ihelin@outlook.com>
2020-10-04 09:03:25 +0800

    update

--- 微信自动抢红包.js
+++ 微信自动抢红包.js
@@ -1,49 +1,64 @@
-auto.waitFor();
+
 
 start();
 
 function start() {
-
-    toastLog("开始监听红包中……");
+    toastLog("监听红包中");
 
     threads.start(function () {//在子进程中运行监听事件
         events.observeNotification();
         events.onNotification(function (notification) {
             if (notification.getPackageName() === 'com.tencent.mm' && notification.getText().indexOf("[微信红包]") > -1) {
                 notification.click();
+
                 grab();
+
             }
+
         });
     });
 
+
     threads.start(function () {
+
         while (true) {
-            if (currentActivity() === "com.tencent.mm.ui.LauncherUI" && id("b4r").findOne(200) !== null) {
+            if (currentActivity() === "com.tencent.mm.ui.LauncherUI" && id("dk_").findOne(200) !== null) {
                 if (textContains("[微信红包]").find().size() > 0) {
                     textContains("[微信红包]").findOne().parent().parent().parent().parent().click();
                     grab();
                 }
             }
-            sleep(3000);
+            // textContains("[微信红包]").findOne().parent().parent().parent().parent().click();
+
+            // toast(111);
+            sleep(1000);
         }
+
     });
+
+    // threads.start(function () {
+    //     // console.show();
+    //     while (currentActivity() === "com.tencent.mm.ui.LauncherUI" && id("l1").findOne(200) !== null) {
+    //         textContains("[微信红包]").find().forEach(child => {
+    //             child.parent().parent().parent().parent().click();
+    //             grab();
+    //         });
+    //         sleep(1000);
+    //     }
+
+    // });
 }
 
-//抢红包函数
 function grab() {
-    waitForActivity("com.tencent.mm.ui.LauncherUI", 50);
-    log('进入聊天页。')
-    var hb = id("al7").find();
+    waitForPackage("com.tencent.mm", 10);
+    waitForActivity("com.tencent.mm.ui.LauncherUI");
+
+    var hb = id("atb").find();
     if (hb.size() > 0) {
         hb[hb.size() - 1].click();
         waitForActivity("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyNotHookReceiveUI", 10);
         desc("开").findOne().click();
-        sleep(3000);
-        back();
-        back();//暂时这么写
     } else {
-        toastLog("手慢了。或者红包id修改了，请联系开发者！");
+        toast("手慢了");
     }
 }
-
-
